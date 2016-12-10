fc318d6b7bf75c8716af9e1e818dac57593723f1
Milan Sisovic <milan.sisovic@p3-group.com>
2016-12-10 13:44:38 +0100

    clean index

--- index.php
+++ index.php
@@ -1,36 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-        <meta charset="utf-8">
-        <title>Тестирамо се!</title>
-</head>
-<body>
-<div>
-<div>
 <?php
-
-function calc_string( $mathString ){
-$doCalc = create_function("", "return (" . $mathString . ");" );
-return $doCalc();
-}
-
-function mat1($a, $b) {
-        echo( "<h2>".$a . " + " . $b ." = </h2>" );
-        $izracunaj=calc_string("$a + $b");
-        return $izracunaj;
-}
-function mat2($a, $b) {
-        echo( "<h2>".$a . " - " . $b ." = </h2>" );
-        $izracunaj=calc_string("$a - $b");
-        return $izracunaj;
-}
-function mat3($a, $b) {
-        echo( "<h2>".$a . " x " . $b ." = </h2>" );
-        $izracunaj=calc_string("$a * $b");
-        return $izracunaj;
-}
-
-echo("<h3>Израчунај:</h3>");
 $i=rand(1,3);
 $a=rand(0,10);
 $b=rand(0,10);
@@ -39,19 +7,53 @@ if ($a < $b){
         $a=$b;
         $b=$k;
 }
-echo('<form action="index.php" method="POST">
-	<label>');
+function calc_string( $mathString ){
+        $doCalc = create_function("", "return (" . $mathString . ");" );
+        return $doCalc();
+}
+
+function mat1($a, $b){
+        echo( "<h2>".$a . " + " . $b ." = </h2>" );
+        $izracunaj=calc_string("$a + $b");
+        return $izracunaj;
+}
+function mat2($a, $b){
+        echo( "<h2>".$a . " - " . $b ." = </h2>" );
+        $izracunaj=calc_string("$a - $b");
+        return $izracunaj;
+}
+function mat3($a, $b){
+        echo( "<h2>".$a . " x " . $b ." = </h2>" );
+        $izracunaj=calc_string("$a * $b");
+        return $izracunaj;
+}
+
 $rezultat=call_user_func("mat".$i, $a, $b);
-echo('  	<input class="numeric" type="tel" min="0" step="1"/>
+
+echo('<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="utf-8">
+        <title>Тестирамо се!</title>
+</head>
+<body>
+<div>
+<div>
+<h3>Израчунај:</h3>
+<form action="index.php" method="POST">
+	<label>
+                <input class="numeric" type="tel" min="0" step="1"/>
                 <input type="submit" value="Реши"/>
         </label>
 	</form>
-        </div>');
+        </div>
+');
 
 echo("<h2>" . $rezultat ." </h2>");
-
-?>
-</div>
+echo('</div>
 
 </body>
 </html>
+');
+
+?>
